0ef3f4e53ad3f40fa0e582ee22784bfaf4a13808
Tao Dong <taojdcn@gmail.com>
2025-07-11 22:05:18 -0700

    remove line break from header value

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ exports.cache_and_save = function (next, connection) {
     const fromHeader = transaction.header.get('From').replace(/\n+$/, '')
     const senderName = fromHeader.replace(/<[^>]*>/g, '').trim()
     const inReplyTo = transaction.header.get('In-Reply-To').replace(/\n+$/, '')
-    const incomingId = transaction.notes.get('Message-ID')
+    const incomingId = transaction.header.get('Message-ID').replace(/\n+$/, '')
 
     const kMessageBody = {
       emailId,
